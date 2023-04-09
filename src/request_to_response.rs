2cc9ec4383d61d5289a92860e522a4d355053bed
borisp <boris.pigin@gmail.com>
2023-04-09 11:28:45 +0200

    should_produce_http_response_with_http_status_401_in_case_http_request_authorization_header_is_missing - refactor

--- src/request_to_response.rs
+++ src/request_to_response.rs
@@ -8,17 +8,22 @@ fn apply(request: Request<()>) -> Response<()> {
 }
 
 fn produce_status(request: Request<()>) -> StatusCode {
-    return if request.headers().is_empty() || request.headers().get("Authorization").is_none() {
+    return if authorization_failed(request) {
         StatusCode::UNAUTHORIZED
     } else {
         StatusCode::OK
     };
 }
 
+fn authorization_failed(request: Request<()>) -> bool {
+    request.headers().is_empty() || request.headers().get("Authorization").is_none()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
     use std::any::type_name;
+
+    use super::*;
 
     fn type_of<T>(_: &T) -> &str {
         return type_name::<T>();
@@ -43,8 +48,12 @@ mod tests {
     }
 
     #[test]
-    fn should_produce_http_response_with_http_status_401_in_case_http_request_authorization_header_is_missing() {
-        let given_request: Request<()> = Request::builder().header("OTHER", "HEADER").body(()).unwrap();
+    fn should_produce_http_response_with_http_status_401_in_case_http_request_authorization_header_is_missing(
+    ) {
+        let given_request: Request<()> = Request::builder()
+            .header("OTHER", "HEADER")
+            .body(())
+            .unwrap();
 
         let response = apply(given_request);
 
