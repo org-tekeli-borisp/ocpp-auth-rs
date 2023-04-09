74e0dd2432ff53294cef9b066f845fe2d75b41d7
borisp <boris.pigin@gmail.com>
2023-04-09 09:33:48 +0200

    specification should_consume_http_request_and_produce_http_response - given

--- src/request_to_response.rs
+++ src/request_to_response.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod tests {
+    use http::Request;
+
     #[test]
     fn should_consume_http_request_and_produce_http_response() {
+        let _given_request: Request<()> = Request::builder().body(()).unwrap();
     }
 }
