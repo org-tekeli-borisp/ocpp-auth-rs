95638a20c4bf65f60aab999e1bd5d5be13b730e3
borisp <boris.pigin@gmail.com>
2023-04-09 09:37:56 +0200

    specification should_consume_http_request_and_produce_http_response - then

--- src/request_to_response.rs
+++ src/request_to_response.rs
@@ -8,11 +8,18 @@ fn apply(_request: Request<()>) -> Response<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::any::type_name;
+
+    fn type_of<T>(_: &T) -> &str {
+        return type_name::<T>();
+    }
 
     #[test]
     fn should_consume_http_request_and_produce_http_response() {
         let given_request: Request<()> = Request::builder().body(()).unwrap();
 
-        let _response = apply(given_request);
+        let response = apply(given_request);
+
+        assert_eq!("http::response::Response<()>", type_of(&response));
     }
 }
